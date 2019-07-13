0e675bf2918f74acca66fa05b2b6d5974b24ef3f
thaveesit douangaphaivong <casethnova@711-MacBook-Air.local>
2019-07-13 12:31:44 -0500

    Smooth

--- js/index.js
+++ js/index.js
@@ -39,4 +39,70 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]
+["img-src"])
+
+let cta = document.getElementById("cta-img");
+cta.setAttribute('src', siteContent ["main-content"]["middle-img-src"])
+
+let anchors = document.querySelector('nav a');
+
+let anchorInfo = ['Services', 'Product', 'Vision', 'Features','About','Contact'];
+
+anchorInfo.forEach(anchorInfo => {
+  const element = 
+  document.createElement('a')
+  element.textContent = anchorInfo
+  anchors.append(element)
+});
+
+let ctaButton =
+document.querySelector('button');
+ctaCutton.textContent = siteContent['cta']['button'];
+
+let ctaHeading = 
+document.querySelector('h1');
+ctaHeading.textContent = sitContent['cta']['h1'];
+
+let testNodeList =
+document.querySelectorAll('.top-content .text-content h4')
+let nodeListContent =
+document.querySelectorAll('.top-content .text-content p')
+testNodeList[0].textContent = siteContent['main-content,']['features-h4'];
+nodeListContent[0].textContent = siteContent['main-content']['features-content']
+testNodeList[1].textContent = siteContent['main-content']['about-h4'];
+nodeListContent[1].textContent = siteContent['main-content']['about-content']
+
+//bottom content
+
+let bottomNodeListh4 = document.querySelectorAll('.bottom-content .text-content h4')
+bottomNodeListh4[0].textContent = siteContent['main-content']['services-h4']
+bottomNodeListh4[1].textContent = siteContent['main-content']['product-h4']
+bottomNodeListh4[2].textContent = siteContent['main-content']['vision-h4']
+
+
+let bottomNodeListContent = document.querySelectorAll('.bottom-content .text-content p')
+bottomNodeListContent[0].textContent = siteContent['main-content']['services-content']
+bottomNodeListContent[1].textContent = siteContent['main-content']['product-content']
+bottomNodeListContent[2].textContent = siteContent['main-content']['vision-content']
+
+//contact
+
+let contacth4 = document.querySelector('.contact h4');
+contacth4.textContent = siteContent['contact']['contact-h4'];
+
+let contactInfo = ['123 Way 456 Street Somewhere, USA','1 (888) 888-8888','sales@greatidea.io'];
+
+let contactP = document.querySelector('.contact p');
+
+contactInfo.forEach(contactInfo => {
+  const element = document.createElement('li')
+  element.textContent = contactInfo
+  contactP.append(element)
+})
+
+//footer
+
+let footer = document.querySelector('footer p');
+footer.textContent = siteContent['footer']['copyright'];
+
